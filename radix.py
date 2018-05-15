32839b8709dfe31c9ee6c4e2e1d4882e09e1ad9c
B-iglesia <bradiglesia@gmail.com>
2018-05-15 12:46:59 -0700

    Continuing the radix sort algorithm

--- radix.py
+++ radix.py
@@ -1,1 +1,38 @@
+from queue import *
 #radix
+#this little function is ripped straight from the book.
+#It's use tis to find the Least Significant Digits by tossing items into queues
+#instead of using a separate file from the internet (though that wouldn't be
+#too hard) it will just be assortments of unsorted lists of strings that will be 
+#sorted
+def charAt(s,i):
+    if len(s) - 1 < i:
+        return " "
+    return s[i]
+
+def find_longest(los):
+    longest = 0
+    for item in los:
+        if len(item) > longest:
+            longest = len(item)
+    return longest
+
+def radixsort(unsortedlst, size):
+    longest = find_longest(unsortedlst)
+    
+    mainQueue = Queue()
+    queueList = [Queue() for i in range(256)]
+    for item in unsortedlst:
+        mainQueue.enqueue(charAt(item, longest))
+    while not mainQueue.isEmpty():
+        word = mainQueue.dequeue()
+        
+    
+
+def main():
+    alist = input("Please enter a list of words")
+    alist = alist.split()
+    radixsort(alist, len(alist))
+    print(alist)
+if __name__ == "__main__":
+    main()
