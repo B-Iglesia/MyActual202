3a4b18fc0533048b117f351a9a730b547517690b
B-iglesia <bradiglesia@gmail.com>
2018-05-25 13:23:58 -0700

    Got levelorder, now to get everything else

--- binarytreevis.py
+++ binarytreevis.py
@@ -1,3 +1,6 @@
+from queue import *
+from stack import *
+
 class BinarySearchTree:
     class __Node:
         def __init__(self,val,left=None,right=None):
@@ -32,68 +35,25 @@ class BinarySearchTree:
         #The smallest value will always be located to the left of the root, so
         #only goes down the left of the tree, traversing until
         #it hits "none" meaning that value is the smallest (no left leaf nodes)
-        def minValue(self,node):
-            current = node
+        def leftminval(self):
+            current = self
             
             while current.left is not None:
                 current = current.left
             
-            return current
+            return current.getVal()
         
-        def delete(self,data):
-            if self is None:
-                return None
-            #determines where to search for the item in the left or right 
-            #sub trees
-            if data < self.val:
-                self.left = self.left.delete(data)
-            elif data > self.val:
-                self.right = self.right.delete(data)
-            
-            else:
-                #deletes a node with one child
-                if self.left is None:
-                    temp = self.right
-                    self = None
-                    return temp
-                elif self.right is None:
-                    temp = self.left
-                    self = None
-                    return temp
-                
-                #deleting a node with two children
-                temp = self.minValue(self.right)
-                self.val = temp.val
-                self.right = self.right.delete(temp.val)
-                
-                
-                
-            return self
+        def rightminval(self):
+            current = self
+            while current.right is not None:
+                current = current.right
+                if current.left != None:
+                    current = current.left
+                    break
+            return current.getVal()
+        #when a user deletes a value higher in a tree, the left child should
+        #become the new val where the old one was.
         
-        def preorder(self):
-            if self:
-                print(str(self.val))
-                if self.left:
-                    self.left.preorder()
-                if self.right:
-                    self.right.preorder()
-        
-        def postorder(self):
-            if self:
-                if self.left:
-                    self.left.postorder()
-                if self.right:
-                    self.right.postorder()
-                print(str(self.val))
-        
-        def inorder(self):
-            if self:
-                if self.left:
-                    self.left.inorder()
-                print(str(self.val)) 
-                if self.right:
-                    self.right.inorder()
-                       
         #Gets all items in ascending order. Does an inorder traversal
         #of the nodes of the tree
         def __iter__(self):
@@ -106,10 +66,34 @@ class BinarySearchTree:
             if self.right != None:
                 for elem in self.right:
                     yield elem
+        
+        def __repr__(self):
+            return str(self.getVal())
+        
+        def levelorder(root):
+            if root is None:
+                return []
+            q = Queue()
+            q.enqueue(root)
+            lol = []
+            while not q.isEmpty():
+                a = q.dequeue()
+                if a.getLeft() != None:
+                    q.enqueue(a.left)
+                if a.getRight() != None:
+                    q.enqueue(a.right)
+                lol.append(a.getVal())
+            return lol        
+        def preorder(self):
+            if self == None:
+                return []
+            
+            
     def __init__(self, contents = []):
         self.root = None
         for i in contents:
             self.insert(i)
+
     def insert(self,val):
         
         #the __insert function is recursive and is not passed a self parameter. It is
@@ -140,46 +124,39 @@ class BinarySearchTree:
             return self.root.contains(data)
         return False
     
-    def delete(self,data):
-        if self.root is not None:
-            return self.root.delete(data)
+    def delete(self,val):
+        pass
+    def leftminvalue(self):
+        return self.root.leftminval()
+    def rightminvalue(self):
+        return self.root.rightminval()
+        
             
     
     def preorder(self):
-        print("PreOrder")
-        self.root.preorder()
-    
+        return self.root.preorder()
     def postorder(self):
-        print("PostOrder")
-        self.root.postorder()
+        return self.root.postorder()
     def inorder(self):
-        print("InOrder")
-        self.root.inorder()
+        return self.root.inorder()
+    def levelorder(self):
+        return self.root.levelorder()
 
 
 
 
 def main():
-    #s = input("Enter a list of numbers: ")
-    #lst = s.split()
     tree = BinarySearchTree([1,3,4,2,6,5,8,7,9])
     
-    #for x in lst:
-        #tree.insert(float(x))
-    #for x in tree:
-       # print(x)
-    tree.preorder()
-    tree.postorder()
-    tree.inorder()
-    print(1 in tree)
-    tree.delete(1)
-    tree.delete(8)
-    #tree.delete(9)
-    #tree.delete(7)
-    print(8 in tree , "me")
-    print(9 in tree)
-    print(7 in tree)
+ 
+
+    print(tree.leftminvalue())
+    print(tree.rightminvalue())
+    tree.delete(2)
+    for i in tree:
+        print(i)
+    print(tree.levelorder())
+    print(tree.preorder())
     
-    #print(1 in tree)
 if __name__ == "__main__":
     main()
