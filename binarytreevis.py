0bb0d513ccf7d1a0d3506f749fdf49e4893aabc1
B-iglesia <bradiglesia@gmail.com>
2018-05-28 17:50:12 -0700

    Delete is OK

--- binarytreevis.py
+++ binarytreevis.py
@@ -1,6 +1,11 @@
 from queue import *
 from stack import *
-
+from turtle import *
+import tkinter.messagebox
+import tkinter
+from tkinter import *
+screenMin = 0
+screenMax = 300
 class BinarySearchTree:
     class __Node:
         def __init__(self,val,left=None,right=None):
@@ -85,24 +90,28 @@ class BinarySearchTree:
         def __repr__(self):
             return str(self.getVal())
         def delete_helper(self, val):
-            if self == None:
+            if self == None or self.val == None:
                 return self
             elif val < self.getVal():
-                self.left = self.left.delete_helper(val)
+                if self.left != None:
+                    self.left = self.left.delete_helper(val)
             elif val > self.getVal():
-                self.right = self.right.delete_helper(val)
+                if self.right != None:
+                    self.right = self.right.delete_helper(val)
             else:
                 if self.no_children():
                     del(self)
                     self = None
                     
                 elif self.left == None:
-                    temp = self
-                    self = self.right
+                    temp = self.right.minval()
+                    self.setVal(temp.getVal())
+                    self.right = self.right.delete_helper(temp.getVal())
                     del(temp)
                 elif self.right == None:
-                    temp = self
-                    self = self.left
+                    temp = self.left.minval()
+                    self.setVal(temp.getVal())
+                    self.left = self.left.delete_helper(temp.getVal())
                     del(temp)
                 else:
                     temp = self.right.minval()
@@ -185,16 +194,18 @@ class BinarySearchTree:
         else:
             return [].__iter__()
     
+    
+    
+    def delete(self, val):
+            
+        return self.root.delete_helper(val)
     def __contains__(self, data):
         return self.contains(data)
     
     def contains(self,data):
         if self.root:
             return self.root.contains(data)
-        return False
-    
-    def delete(self, val):
-        return self.root.delete_helper(val)
+        return False    
     def minvalue(self):
         return self.root.minval()
     def rightminvalue(self):
@@ -215,20 +226,67 @@ class BinarySearchTree:
         return self.root.levelorder()
 
 
-
-
+class BSTVis(tkinter.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.pack()
+        self.pack()
+        self.buildWindow()    
+        self.paused = False
+        self.stop = False
+        self.running = False
+        self.locked = False
+    def buildWindow(self):
+        cv = ScrolledCanvas(self, 600,600,600,600)
+        cv.pack(side = tkinter.LEFT)
+        t = RawTurtle(cv)
+        screen = t.getscreen()
+        screen.tracer(100000)
+        
+        screen.setworldcoordinates(screenMin,screenMin,screenMax,screenMax)
+        screen.bgcolor("white")
+        t.ht()
+        
+        frame = tkinter.Frame(self)
+        frame.pack(side = tkinter.RIGHT,fill=tkinter.BOTH)
+        
+        def quitHandler():
+            self.master.quit()
+        quitButton = tkinter.Button(frame, text = "Quit", command =quitHandler)
+        quitButton.pack()
+        
+        def entryHandler():
+            pass
+        e = Entry(frame)
+        e.pack()
+        
+        insertButton = tkinter.Button(frame, text = "Insert")
+        insertButton.pack()
+        
+        containsButton = tkinter.Button(frame,text = "Contains?")
+        containsButton.pack()
+        
+        
 def main():
     tree = BinarySearchTree(contents = [10, 7, 13, 4, 8,7.5,12, 14,2,5,15, 16])
     tree2 = BinarySearchTree(contents=[0, 1, 2])
- 
+    tree3 = BinarySearchTree(contents=[0,1,2,3,4,5])
     tree2.delete(0)
+    print(tree2.delete(0))
     print(0 in tree2)
-    print(10 in tree)
-    #tree.delete(2)
-    tree.delete(10)
+    
+    for i in tree2:
+        print(i)
+ #   print(tree2.levelorder())
+  #  tree.delete(15)
+    
+#    rot = tkinter.Tk()
+ #   rot.title("Binary Search Tree Visualizer")
+  #  application = BSTVis(rot)
+   # application.mainloop()
     print(tree.levelorder(), "levelorder")
-    print(tree.inorder(), "inorder")
-    print(tree.preorder(), "preorder")
-    print(tree.postorder(), "postorder")
+    #print(tree.inorder(), "inorder")
+    #print(tree.preorder(), "preorder")
+    #print(tree.postorder(), "postorder")
 if __name__ == "__main__":
     main()
