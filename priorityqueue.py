55b9818a077f22a60061d35aae29fb7c428e58e4
B-iglesia <bradiglesia@gmail.com>
2018-06-03 22:46:30 -0700

    Made it so that .front() returns a strings of the front item

---
+++ priorityqueue.py
@@ -0,0 +1,73 @@
+class PriorityQueue:
+    class __Node:
+        def __init__(self, data, priority, next = None):
+            self.data = data
+            self.priority = priority
+            self.next = next
+        def getD(self):
+            return self.data
+        def setD(self, d):
+            self.data = d
+        def getP(self):
+            return self.priority
+        def setP(self,p):
+            self.priority = p
+        def getN(self):
+            return self.next
+        def setN(self,n):
+            self.next = n
+        def __repr__(self):
+            return str(self.getD())
+            
+    def __init__(self):
+        self.root = None
+        self.count = 0
+    def isEmpty(self):
+        return self.count == 0
+    def __len__(self):
+        return self.count
+    def enqueue(self,d,p):
+        def __enqueue(root,d,p):
+            if root == None:
+                return PriorityQueue.__Node(d,p)
+            newNode = PriorityQueue.__Node(d,p)
+            if newNode.priority > root.priority:
+                
+                temp = root
+                root = newNode
+                root.next = temp
+            else:
+                root.next = newNode
+            return root
+        self.root = __enqueue(self.root,d,p)
+        self.count+=1
+    def dequeue(self):
+        temp = self
+        self.root = self.root.getN()
+        
+        self.count -=1
+        return temp
+    def front(self):
+        return str(self.root)
+    def __repr__(self):
+        return str(list(self))
+    def __iter__(self):
+        current = self.root
+        while current != None:
+            yield current.getD()
+            current = current.getN()
+        
+def main():
+    p = PriorityQueue()
+    for i in range(11):
+        p.enqueue(i**2,i**2)
+    print(p.count)
+    print(p)
+    p.dequeue()
+    print(p)
+    print(p.front())
+    while p:
+        p.dequeue()
+    print(p.isEmpty())
+if __name__ == "__main__":
+    main()
